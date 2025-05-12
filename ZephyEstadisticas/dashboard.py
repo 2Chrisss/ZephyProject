8abb56622e265ddbf6199101677df1fbb3d84a12
Chriss <ccallef@udd.cl>
2025-05-12 00:12:48 -0400

    arreglo dashboard

--- ZephyEstadisticas/dashboard.py
+++ ZephyEstadisticas/dashboard.py
@@ -4,6 +4,7 @@ def obtener_estadisticas():
     from ZephyReportes.models import Box, Boxprofesional
     from datetime import time, datetime, timedelta
     from django.db.models import Count
+
     box_disponibles = Box.objects.filter(estadobox_idestadobox=1).count()
     box_ocupados = Box.objects.filter(estadobox_idestadobox=2).count()
     box_no_disponibles = Box.objects.filter(estadobox_idestadobox=3).count()
@@ -15,6 +16,8 @@ def obtener_estadisticas():
         time(14, 0), time(14, 30), time(15, 0), time(15, 30), time(16, 0), time(16, 30),
         time(17, 0), time(17, 30)
     ]
+    
+
     ocupacion_por_intervalo = []
     for intervalo in intervalos:
         ocupados = Boxprofesional.objects.filter(
@@ -28,17 +31,20 @@ def obtener_estadisticas():
             'ocupados': ocupados,
             'disponibles': disponibles
         })
+    
     hoy = datetime.now().date()
 
-    boxes_poco_ocupados = Boxprofesional.objects.filter(
+
+    boxes_poco_ocupados = list(Boxprofesional.objects.filter(
         fechaasignacion=hoy,
     ).values(
         'box_idbox__numerobox'
     ).annotate(
         total_ocupaciones=Count('idboxprofesionalcol')
-    ).order_by('total_ocupaciones')[:5]
+    ).order_by('total_ocupaciones')[:5])  
 
     if len(boxes_poco_ocupados) < 5:
+
         boxes_sin_ocupaciones = Box.objects.exclude(
             idbox__in=Boxprofesional.objects.filter(
                 fechaasignacion=hoy
@@ -46,6 +52,7 @@ def obtener_estadisticas():
         ).values('numerobox')[:5 - len(boxes_poco_ocupados)]
         
         for box in boxes_sin_ocupaciones:
+  
             boxes_poco_ocupados.append({
                 'box_idbox__numerobox': box['numerobox'],
                 'total_ocupaciones': 0
@@ -56,13 +63,6 @@ def obtener_estadisticas():
         'data': [box['total_ocupaciones'] for box in boxes_poco_ocupados]
     }
 
-    return {
-        "box_disponibles": box_disponibles,
-        "box_ocupados": box_ocupados,
-        "box_no_disponibles": box_no_disponibles,
-        "ocupacion_por_intervalo": ocupacion_por_intervalo,
-        "top_poco_ocupados": top_poco_ocupados
-    }
     return {
         "box_disponibles": box_disponibles,
         "box_ocupados": box_ocupados,
